267d4250871d167e1c1103020a6d96683aa0a3a1
zvdv <156561301+zvdv@users.noreply.github.com>
2024-12-29 13:18:57 -0800

    Accelerometer

---
+++ accel/Debug/Drivers/BSP/STM32F4-Discovery/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (12.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Drivers/BSP/STM32F4-Discovery/stm32f4_discovery.c \
+../Drivers/BSP/STM32F4-Discovery/stm32f4_discovery_accelerometer.c 
+
+OBJS += \
+./Drivers/BSP/STM32F4-Discovery/stm32f4_discovery.o \
+./Drivers/BSP/STM32F4-Discovery/stm32f4_discovery_accelerometer.o 
+
+C_DEPS += \
+./Drivers/BSP/STM32F4-Discovery/stm32f4_discovery.d \
+./Drivers/BSP/STM32F4-Discovery/stm32f4_discovery_accelerometer.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/BSP/STM32F4-Discovery/%.o Drivers/BSP/STM32F4-Discovery/%.su Drivers/BSP/STM32F4-Discovery/%.cyclo: ../Drivers/BSP/STM32F4-Discovery/%.c Drivers/BSP/STM32F4-Discovery/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F407xx -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I../Drivers/BSP/STM32F4-Discovery -I../Drivers/BSP/Components/Common -I../Drivers/BSP/Components/lis3dsh -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Drivers-2f-BSP-2f-STM32F4-2d-Discovery
+
+clean-Drivers-2f-BSP-2f-STM32F4-2d-Discovery:
+	-$(RM) ./Drivers/BSP/STM32F4-Discovery/stm32f4_discovery.cyclo ./Drivers/BSP/STM32F4-Discovery/stm32f4_discovery.d ./Drivers/BSP/STM32F4-Discovery/stm32f4_discovery.o ./Drivers/BSP/STM32F4-Discovery/stm32f4_discovery.su ./Drivers/BSP/STM32F4-Discovery/stm32f4_discovery_accelerometer.cyclo ./Drivers/BSP/STM32F4-Discovery/stm32f4_discovery_accelerometer.d ./Drivers/BSP/STM32F4-Discovery/stm32f4_discovery_accelerometer.o ./Drivers/BSP/STM32F4-Discovery/stm32f4_discovery_accelerometer.su
+
+.PHONY: clean-Drivers-2f-BSP-2f-STM32F4-2d-Discovery
+
